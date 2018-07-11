4217ea15a41935f4637b159c7e8427fd9a54cd54
Domino <zhikhanastasya@gmail.com>
2018-07-11 10:44:11 +0300

    Chore (Logic.Task1)

--- Logic.Task1.Tests/StringExtensionTests.cs
+++ Logic.Task1.Tests/StringExtensionTests.cs
@@ -15,9 +15,7 @@ namespace Logic.Task1.Tests
         [TestCase("7FFFFFFF", 16, int.MaxValue)]
         public void ToDecimal_String_CorrectResult(string value, int scale, int expected)
         {
-            var notation = new Notation(scale);     
-
-            int actual = value.ToDecimal(notation);
+            int actual = value.ToDecimal(scale);
 
             Assert.AreEqual(expected, actual);
         }
@@ -27,20 +25,13 @@ namespace Logic.Task1.Tests
         [TestCase("764241", 2)]
         [TestCase("123", 3)]
         [TestCase("H123A", 16)]
+        [TestCase("111111100000000000000001111111111", 2)]
+        [TestCase("11111111111111111111111111111111", 2)]
+        [TestCase("764241", 1)]
+        [TestCase("764241", 17)]
         public void ToDecimal_UncorrectParams_ThrowArgumentException(string value, int scale)
         {
-            var notation = new Notation(scale);
-
-            Assert.Throws<ArgumentException>(() => value.ToDecimal(notation));
-        }
-
-        [TestCase("11111111111111111111111111111111", 2)]
-        [TestCase("111111100000000000000001111111111", 2)]
-        public void ToDecimal_UncorrectParams_ThrowOverflowException(string value, int scale)
-        {
-            var notation = new Notation(scale);
-
-            Assert.Throws<OverflowException>(() => value.ToDecimal(notation));
+            Assert.Throws<ArgumentException>(() => value.ToDecimal(scale));
         }
     }
 }
